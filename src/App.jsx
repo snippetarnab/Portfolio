86f175bfc975f3a21e2f21bb846e43e4b848923f
snippetarnab <arnabvivo2020@gmail.com>
2025-04-01 21:28:01 +0530

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -191,6 +191,66 @@ const Portfolio = () => {
           <FaMoon />
         </button> */}
       </div>
+
+      {/* Footer */}
+      <footer className="footer footer-horizontal footer-center bg-black text-gray-400  p-10 flex justify-center max-w-md mx-auto md:flex-row">
+        <aside className="flex flex-col items-center ">
+          <div className="flex flex-row gap-4 ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-code-xml-icon lucide-code-xml w-4"
+            >
+              <path d="m18 16 4-4-4-4" />
+              <path d="m6 8-4 4 4 4" />
+              <path d="m14.5 4-5 16" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-github-icon lucide-github w-4"
+            >
+              <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+              <path d="M9 18c-4.51 2-5-2-7-2" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-square-terminal-icon lucide-square-terminal w-4"
+            >
+              <path d="m7 11 2-2-2-2" />
+              <path d="M11 13h4" />
+              <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
+            </svg>
+          </div>
+          <p className="text-[9px] md:text-sm">
+            Copyright © {new Date().getFullYear()} - All right reserved to
+            @snippetarnab{" "}
+          </p>
+        </aside>
+      </footer>
+      {/* Footer End */}
     </div>
   );
 };
